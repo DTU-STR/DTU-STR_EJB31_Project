c27b84f9d900ec402847e7e7f6293420ca25ed4e
dtomsu <daniel@tomsu.de>
2012-02-13 01:19:35 +0100

    added build for deployment, Main class, not working, because in JBoss 7 there are major changes to ejb deployment and calling

---
+++ JavaEE_31/src/client/kap02/ErstesBeanMain.java
@@ -0,0 +1,33 @@
+package client.kap02;
+
+import java.util.Properties;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.rmi.PortableRemoteObject;
+
+import server.kap02.ErstesBeanRemote;
+
+public class ErstesBeanMain {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		try {
+			Properties p = new Properties();
+			p.put(Context.INITIAL_CONTEXT_FACTORY, "org.jnp.interfaces.NamingContextFactory");
+			p.put(Context.PROVIDER_URL, "jnp://localhost:1099");
+			Context ctx = new InitialContext(p);
+			Object ref = ctx.lookup("java:global/ErstesBean/ErstesBean");
+			ErstesBeanRemote ebr = (ErstesBeanRemote) PortableRemoteObject.narrow(ref,  ErstesBeanRemote.class);
+			
+			System.out.println("5 + 8 = " + ebr.addieren(5, 8));
+		} catch (NamingException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+}
